d881bae6d9cb1881ebbce0a9de3362ab1b7fbe67
LutenkoMikhail <lamprologus2011@gmail.com>
2020-01-31 15:43:37 +0200

    Fix bag and refactoring

--- index.php
+++ index.php
@@ -16,20 +16,23 @@ echo "<h3>1.1 An evil clown </h3>";
  */
 function convertingSmiley(string $basicString): string
 {
-    $patterns=array('/:\)\)/', '/:\(\(/');
+    $patterns=array('/:\){2,}/', '/:\({2,}/');
     $replace = array (':)', ':(');
     return preg_replace($patterns, $replace, $basicString);
 }
 
-$basicString = 'An evil and gloomy :((, bald clown walks through the woods in winter.:))';
+$basicString = 'An evil :((( and gloomy :(((, bald :( clown walks through :)))) the woods in winter. :)';
 
-echo "Test line: {$basicString} <br>" ;
+echo "Test line: {$basicString} ".PHP_EOL ;
 echo 'String after conversion :'.convertingSmiley($basicString);
 
-
-
-
-
+/* T.E comments:
+ * 1.1 This method should be able to leave 1 bracket,
+ *  even if there are 10 or more brackets in a row in the text,
+ *  this implementation removes only the second bracket from the smiles,
+ *  if I change the number of brackets more than 2, this implementation will not work.
+ * 1.2 If we have a poor understanding of regular expressions, then other methods can be used to accomplish this task.
+ */
 
 
 
